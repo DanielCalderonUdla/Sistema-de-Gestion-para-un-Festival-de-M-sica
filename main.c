4d4b9c348aa8db89a3efc0511ef3fde2209c4ac3
Cisco <cisco@udla-ec.int>
2023-07-12 08:46:16 -0500

    Añadido opción estadisticas

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 #define vipMAX 500
 #define backstageMAX 100
 
+#define precioGeneral 100
+#define precioVip 250
+#define precioBackstage 500
+
 #define user1 "Daniel"
 #define pass1 1234
 
@@ -16,7 +20,7 @@
 
 int main(void){
     
-int passLeer, opcion, contador = 0, confirmación = 1, entradas, general = 0, vip = 0, backstage = 0;
+int passLeer, opcion, opcion2, contador = 0, confirmación = 1, entradas = 0, general = 0, vip = 0, backstage = 0, ingresoGeneral, ingresoVip, ingresoBackstage, ingresoTotal, entradasTotal;
 char userLeer[20];
 
 do{
@@ -50,11 +54,11 @@ do{
     switch (opcion)
     {
     case 1:
-        printf("Venta de entradas\n");
+        printf("Venta de Entradas\n");
         printf("¿Que tipo de entrada desea comprar?\n 1. General\n 2. VIP\n 3. Backstage\n");
-        scanf("%d", &opcion);
-
-        switch (opcion)
+        scanf("%d", &opcion2);
+        
+        switch (opcion2)
         {
         case 1:
             printf("General\n");
@@ -70,7 +74,7 @@ do{
                     printf("Actualmente no se dispone de esa cantidad de entradas, vuelva a ingresar otra cantidad\nEntradas disponibles %d\n", (generalMAX - general));
                     confirmación = 1;
                 }
-                if (general = generalMAX)
+                if (general == generalMAX)
                 {
                     confirmación = 0;
                 }
@@ -93,7 +97,7 @@ do{
                     printf("Actualmente no se dispone de esa cantidad de entradas, vuelva a ingresar otra cantidad\nEntradas disponibles %d\n", (vipMAX - vip));
                     confirmación = 1;
                 }
-                if (vip = vipMAX)
+                if (vip == vipMAX)
                 {
                     confirmación = 0;
                 }
@@ -108,14 +112,14 @@ do{
                 scanf("%d", &entradas);
                 if (entradas <= (backstageMAX - backstage))
                 {
-                    general += entradas;
+                    backstage += entradas;
                     printf("Transacción Exitosa, se han comprado %d entradas\n", entradas);
                     confirmación = 0;
                 } else{
                     printf("Actualmente no se dispone de esa cantidad de entradas, vuelva a ingresar otra cantidad\nEntradas disponibles %d\n", (backstageMAX - backstage));
                     confirmación = 1;
                 }
-                if (backstage = backstageMAX)
+                if (backstage == backstageMAX)
                 {
                     confirmación = 0;
                 }
@@ -130,11 +134,23 @@ do{
         break;
 
     case 2:
+        printf("Estadísticas de Ventas\n");
+        ingresoGeneral = precioGeneral * general;
+        ingresoVip = precioVip * vip;
+        ingresoBackstage = precioBackstage * backstage;
+        ingresoTotal = ingresoGeneral + ingresoVip + ingresoBackstage;
+        entradasTotal = general + vip + backstage;
+
+        printf("Total entradas de General vendidas: %d\nTotal entradas de VIP vendidas: %d\nTotal entradas de Backstage vendidas: %d\nTotal entradas vendidas: %d\n", general, vip, backstage, entradasTotal);
+        printf("Ingresos entradas General: %d\nIngresos entradas VIP: %d\nIngresos entradas Backstage: %d\nIngresos Totales: %d\n", ingresoGeneral, ingresoVip, ingresoBackstage, ingresoTotal);
 
         break;
 
+    case 3:
+        printf("Gracias por su visita");
+
     default:
-        printf("Opción no existente\n");
+        printf("Opción no existente, vuelva a ingresar\n");
         break;
     }
 
