81de9565fc4b4f866ce1aac6d87726684b5b5ada
Cisco <cisco@udla-ec.int>
2023-07-12 07:18:43 -0500

    Declaración de variables iniciales

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+#define generalMAX 1000
+#define vipMAX 500
+#define backstageMAX 100
+
+#define user1 "Daniel"
+#define pass1 1234
+
+#define user2 "Roberto"
+#define pass2 1234
+
+#define user3 "Nico" 
+#define pass3 1234
+
+int main(void){
+    
+int passLeer, opcion, contador = 0;
+char userLeer[20];
+
+
+
+
+
+}
